d01e9c2293dbad781cd5ebdd21208dd4fa94d2c9
Zsolt Harmath <harmathzs@gmail.com>
2025-09-17 08:05:44 +0200

    Show error page

--- react-app/src/backend/express.cjs
+++ react-app/src/backend/express.cjs
@@ -1,5 +1,10 @@
 const express = require('express')
+const path = require('path')
 const app = express()
+
+app.use((req, res)=>{
+    res.status(404).sendFile(path.join(__dirname, 'error.html'))
+})
 
 const port = 3333
 app.listen(port, ()=>{
